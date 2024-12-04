129685bee1c7378da044d79539de8a8da5853e02
Alan Linares <alanzl2x10@hotmail.com>
2024-12-04 10:36:41 -0300

    refatorado-navtab

--- main/javascript/module/navegaocaotab.js
+++ main/javascript/module/navegaocaotab.js
@@ -1,26 +1,34 @@
 // NAVEGACÃO POR TABS
-export default function ativaNavegacao() {
+export default class ativaNavegacao {
   // ISOLANDO O CODIGO NO ESCOPO DA FUNCITION
-  const tabMenu = document.querySelectorAll("[data-tab] li");
-  const tabContent = document.querySelectorAll("[data-tab] section");
-  
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
-    // VERIFICA SE TEM TABMENU OU TABCONTENT E SE DENTRO DELES TEM CONTEUDO
-    function activeTab(index) {
-      tabContent.forEach((content) => {
-        // FAZ UM LOOP E REMOVE A CLASS ATIVO NO QUAIS TIVER A CLASS ATIVO
-        content.classList.remove("ativo");
-      });
-      const direcao = tabContent[index].dataset.anime;
-      tabContent[index].classList.add("ativo", direcao); // ADICIONA A CLASS ATIVO
+    constructor (menu,content) {
+      this.tabMenu = document.querySelectorAll(menu);
+      this.tabContent = document.querySelectorAll(content)
+      this.classActive = "ativo"
     }
 
-    tabMenu.forEach((item, index) => {
-      // FAZ UM LOOP POR CADA ELEMENTO DA LISTA PEGA O ITEM E ADICIONA UM EVENTO DE CLICK E UMA FUNCAO QUE ATIVA A FUNCÁO ACTIVETAB
-      item.addEventListener("click", function () {
-        activeTab(index);
-      });
+    activeTab(index) {
+    this.tabContent.forEach((content) => {
+      // FAZ UM LOOP E REMOVE A CLASS ATIVO NO QUAIS TIVER A CLASS ATIVO
+      content.classList.remove(this.classActive);
     });
+    
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.classActive, direcao); // ADICIONA A CLASS ATIVO
   }
-}
+
+    addTabEvent () {
+      this.tabMenu.forEach((item, index) => {
+        // FAZ UM LOOP POR CADA ELEMENTO DA LISTA PEGA O ITEM E ADICIONA UM EVENTO DE CLICK E UMA FUNCAO QUE ATIVA A FUNCÁO ACTIVETAB
+        item.addEventListener("click", () =>  this.activeTab(index))
+      });
+    }
+
+  // iniciar função
+    init () {
+      if (this.tabMenu.length && this.tabContent.length) {
+        this.tabContent[0].classList.add(this.classActive)
+        this.addTabEvent()
+      }
+    }
+ }
